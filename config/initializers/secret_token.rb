aa9c72b6f56770202a891cfd14401061a692a6c0
MarshalJoe <joseph.charles.marshall@gmail.com>
2013-11-25 14:57:48 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bundled::Application.config.secret_key_base = 'd41102b2782f5e69a7d22690559405d5c4247d04cb5f62c4d80d87f24e7744173d6116f50c5b18e1b68a8bb2986fc8d05a1286cc683dcdcf6dcb547df64d40c6'
